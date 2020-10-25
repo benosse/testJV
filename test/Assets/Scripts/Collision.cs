6b7d73abb908459015649623e10ec9047fe2b5ad
benoit zenker <benoit.zenker@gmail.com>
2020-10-25 11:00:18 +0100

    un test de scène avec déplacement et deux zones

---
+++ test/Assets/Scripts/Collision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public GameObject handler;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        handler.GetComponent<ZonesHandler>().setZone(this.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+      handler.GetComponent<ZonesHandler>().leaveZone(this.gameObject);
+    }
+
+    public void playSound() {
+      Debug.Log("playing sound of" + this.gameObject.name);
+    }
+}
---
+++ test/Assets/Scripts/ZonesHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonesHandler : MonoBehaviour
+{
+
+  private GameObject activeZone;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (activeZone != null)
+      {
+        activeZone.GetComponent<Collision>().playSound();
+      }
+    }
+
+    public void setZone(GameObject zone)
+    {
+      Debug.Log(zone.name);
+      Debug.Log("handler setting the zone...");
+
+      activeZone = zone;
+    }
+
+    public void leaveZone(GameObject zone)
+    {
+      Debug.Log("leaving zone");
+      if (activeZone == zone)
+      {
+        activeZone = null;
+      }
+    }
+}
