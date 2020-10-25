a64ec9e0d37d37e5da2181350780976f2d7536f9
benoit zenker <benoit.zenker@gmail.com>
2020-10-25 15:41:16 +0100

    ajout des objets AudioEmitter et ajout de commentaires

--- test/Assets/Scripts/ZonesHandler.cs
+++ test/Assets/Scripts/ZonesHandler.cs
@@ -1,37 +1,40 @@
-﻿using System.Collections;
+/*
+BZ
+Un objet qui gère toutes lez zones du jeu.
+Elle sauvegarde dans quelle zone le joueur se trouve actuellement et commande le lancement audio des zones.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ZonesHandler : MonoBehaviour
 {
 
-  private GameObject activeZone;
-    // Start is called before the first frame update
-    void Start()
-    {
+    //La zone dans laquelle le joueur se trouve
+    private GameObject activeZone;
 
-    }
 
-    // Update is called once per frame
     void Update()
     {
+      //Si le joueur se trouve dans une zone, on demande à la zone de jouer son audio
       if (activeZone != null)
       {
-        activeZone.GetComponent<Collision>().playSound();
+        activeZone.GetComponent<ZoneAudio>().playSound();
       }
     }
 
+    //change la zone active
     public void setZone(GameObject zone)
     {
-      Debug.Log(zone.name);
-      Debug.Log("handler setting the zone...");
-
+      Debug.Log("handler setting the zone..." + zone.name);
       activeZone = zone;
     }
 
+    //quitte la zone actuelle
     public void leaveZone(GameObject zone)
     {
-      Debug.Log("leaving zone");
+      Debug.Log("leaving zone" + activeZone.name);
       if (activeZone == zone)
       {
         activeZone = null;
