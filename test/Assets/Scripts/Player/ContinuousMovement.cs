973ef1ddad97323a702243faab1bd6cda294d789
LinaAbssi <lina.abssi@hotmail.fr>
2020-11-11 17:24:32 +0100

    Ajout de PostProcessing + changement animation selon position.y + attraper des objets +trail render (pas beau)

--- test/Assets/Scripts/Player/ContinuousMovement.cs
+++ test/Assets/Scripts/Player/ContinuousMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ContinuousMovement : MonoBehaviour
 {
     // Vitesse de déplacement du joueur
-    public float speed = 1;
+    public float speed = 50;
    // Declare un input, ici utilisé pour declarer les manettes
     public XRNode inputSource;
     // Gravité Terrestre
@@ -20,12 +20,14 @@ public class ContinuousMovement : MonoBehaviour
     private float fallingSpeed;
     // Casque
     private XRRig rig;
-    
+ 
     private Vector2 inputAxis;
     private CharacterController character;
     // Start is called before the first frame update
     void Start()
     {
+        
+       
         character = GetComponent<CharacterController>();
         rig = GetComponent<XRRig>();
         
@@ -34,8 +36,10 @@ public class ContinuousMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+       
+       InputDevice device =  InputDevices.GetDeviceAtXRNode(inputSource);
        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
     }
 
     private void FixedUpdate() {
@@ -44,9 +48,10 @@ public class ContinuousMovement : MonoBehaviour
         // Recuperer la position de la tête
         Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y,0);
       
-        Vector3 direction = new Vector3(inputAxis.x, 0, inputAxis.y);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
         character.Move(direction * Time.fixedDeltaTime * speed);
         // Gravity
+       
         bool isGrounded = CheckIfGrounded();
         if (isGrounded)
             fallingSpeed = 0;
