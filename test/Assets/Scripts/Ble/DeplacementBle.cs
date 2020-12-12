67750e3badc66130a22a45c542b4bc5e30e0a144
benoit zenker <benoit.zenker@gmail.com>
2020-12-12 19:03:49 +0100

    sahder BleBZ et debut travail sur le terrain

---
+++ test/Assets/Scripts/Ble/DeplacementBle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementBle : MonoBehaviour
+{
+
+    public GestionnaireADSR enveloppe;
+    private Material mat;
+
+
+    void Start()
+    {
+        Debug.Log("ble" + gameObject.name);
+        if (this.enveloppe)
+        {
+            this.enveloppe.EnregistrerDoux(Bouger);
+        }
+        
+        this.mat = gameObject.GetComponent<Renderer> ().material;
+    }
+
+    void Bouger(float valeur)
+    {
+        this.mat.SetFloat("Deplacement", valeur);
+    }
+}
---
+++ test/Assets/Scripts/Ble/GestionnaireTerrain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+BZ
+Les scripts ne s'exécutent pas avec les trees posés via le terrain
+Il faut sûrement reconstruire les trees en tant que gameobject et non treeinstance
+*/
+public class GestionnaireTerrain : MonoBehaviour
+{
+    public GestionnaireADSR enveloppe;
+    private TerrainData data;
+
+    void Start()
+    {
+        this.data = gameObject.GetComponent<Terrain>().terrainData;
+        /*
+        for (int i = 0; i<data.treeInstanceCount; i++)
+        {
+            GameObject prefab = data.treePrototypes[this.data.treeInstances[i].prototypeIndex].prefab;
+            Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+        */
+        //this.data.SetTreeInstances(new TreeInstance[0], false);
+        
+    }
+
+    
+}
